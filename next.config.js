b98866e4486335f3fa3622f5a8ea4c1b22714ca2
Bond <shrekuu@gmail.com>
2024-01-10 21:56:07 +0800

    chore: cleanup; add basic config files

--- next.config.js
+++ next.config.js
@@ -1,4 +1,44 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  skipTrailingSlashRedirect: true,
+  reactStrictMode: false,
+  swcMinify: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+      },
+    ],
+  },
+  async headers() {
+    return [
+      {
+        // Apply these headers to all routes in your application.
+        source: "/:path*",
+        headers: [
+          // https://nextjs.org/docs/advanced-features/security-headers#x-frame-options
+          // prevent clickjacking
+          {
+            key: "X-Frame-Options",
+            value: "SAMEORIGIN",
+          },
+          {
+            key: "Content-Security-Policy",
+            value: "frame-ancestors 'none'",
+          },
+        ],
+      },
+    ];
+  },
+  eslint: {
+    // disable eslint during build phase
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    // disable type-checking during build phase
+    ignoreBuildErrors: true,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
